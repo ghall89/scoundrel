b59cd826f3ebb895b4a732b8471d9748c7c8f1ab
Graham Hall <graham@ghall.dev>
2025-09-29 17:37:37 -0400

    ignore dist/ folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import solid from 'eslint-plugin-solid/configs/typescript';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig(
+  globalIgnores(['dist/']),
   eslint.configs.recommended,
   tseslint.configs.recommended,
   eslintPluginUnicorn.configs.unopinionated,
